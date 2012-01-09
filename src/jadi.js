ca8a9d9db68307b698a8c339ca60af39ac079c43
= <yunxdev@gmail.com>
2012-01-08 21:19:46 -0800

    add node plugin and test(asyn) plugin

--- src/jadi.js
+++ src/jadi.js
@@ -1,4 +1,5 @@
-(function(){
+exports = exports || {};
+exports.jadi = function jadi(){
 /**
 * NO more new keyword! All {}! NO more inheritance! All mixin!
 * Object constructor and function are the same! all are invokables.
@@ -13,17 +14,17 @@ var define = (function createContainer(){
 				throw name+" already declared!"
 			}
 		},
-		merge : function(src, des){
+		merge : function(src, des, soft){
 			if(src === undefined){
 				return;
 			}
 			for(var property in src){
-				this.assertDeclaration(des[property], name+"."+property);
+				this.assertDeclaration(des[property], property);
 				des[property] = src[property];
 			}
 			return des;
 		},
-		reflectedGet : function(obj,path,parent){
+		resolvePath : function(obj,path,parent){
 			var fullPath = path.split(".");
 			var des = obj;
 			for(var i=0; i < fullPath.length - 1; i++){
@@ -37,17 +38,6 @@ var define = (function createContainer(){
 				return des;
 			}
 			return des[fullPath[fullPath.length-1]];
-		},
-		isPrimitive : function(val){
-			var primitives = {
-				number : true,
-				boolean : true,
-				string : true
-			};
-			return typeof val in primitives;
-		},
-		isFunction : function(val){
-			return typeof val === "function";
 		}
 	};
 	var container = {};
@@ -73,7 +63,7 @@ var define = (function createContainer(){
 	var contextCreator = function Context(name,contextType,fn,internal){
 		internal = internal === undefined? true : internal;
 		var fullPath = name.split(".");
-		var des = utils.reflectedGet(container,name,true);
+		var des = utils.resolvePath(container,name,true);
 		var ending = fullPath[fullPath.length-1];
 		if(utils.isModule(ending,contextType)){
 			var existingModule = des[ending] || (des[ending] = {});
@@ -105,10 +95,16 @@ define("architecture",define.MODULE,function(m,container){
 	}
 }).bind(define);
 
-define.module("utils", function(module){
+define.module("utils", function utils(module){
 	return {
 		isString : function(val){
 			return typeof val === "string";
+		},
+		isArray : function(val){
+			return typeof val === "object" && val.length !== undefined;
+		},
+		isObject : function(val){
+			return typeof val === "object";
 		},
 		parseColon : function(str){
 			if(str === undefined){
@@ -119,33 +115,64 @@ define.module("utils", function(module){
 				prefix : val[0],
 				subfix : val[1]
 			}
+		},
+		isPrimitive : function(val){
+			var primitives = {
+				number : true,
+				boolean : true,
+				string : true
+			};
+			return typeof val in primitives;
+		},
+		isFunction : function(val){
+			return typeof val === "function";
+		},
+		identity : function(bd){
+			if(bd.id !== undefined){
+				return "id:" + bd.id;
+			}
+			if(bd.path !== undefined){
+				return "path:" + bd.path;
+			}
+			return "anonymous bean";
 		}
 	};
 });
 
-define.clazz("factory.BeanDefinition", function BeanDefinition(bd,utils,base){
+define.clazz("factory.BeanDefinition", function BeanDefinition(bd,utils){
 	var EMPTY_ARRAY = [];
 	var EMPTY_PROPERTY = {};
-	function Property(p){
-		if(p === undefined){
-			return EMPTY_PROPERTY;
+	
+	function ParseProperty(prop){
+		if(prop === undefined){
+			return undefined;
 		}
-		function ParseProperty(prop){
-			if(prop !== undefined && typeof prop === "object"){
-				beanDef = BeanDefinition(prop,base);
+		if(utils.isArray(prop)){
+			return prop;
+		}
+		if(utils.isObject(prop)){
+			if("path" in prop){
+				beanDef = BeanDefinition(prop,utils);
 				beanDef.scope = "prototype";
 				return beanDef;
 			}
-			if(typeof prop === "string"){
-				var parsed = utils.parseColon(prop);
-				if(parsed && parsed.prefix === "path"){
-					return parsed.prefix+":"+AddBase(parsed.subfix);
-				}
-				return prop;
+			return prop;
+		}
+		if(utils.isString(prop)){
+			var parsed = utils.parseColon(prop);
+			if(parsed && parsed.prefix === "path"){
+				return parsed.subfix;
 			}
-			if(utils.isPrimitive(prop)){
-				return prop;
-			}
+			return prop;
+		}
+		if(utils.isPrimitive(prop)){
+			return prop;
+		}
+	}
+	
+	function Property(p){
+		if(p === undefined){
+			return EMPTY_PROPERTY;
 		}
 		for(var name in p){
 			var prop = p[name];
@@ -157,46 +184,49 @@ define.clazz("factory.BeanDefinition", function BeanDefinition(bd,utils,base){
 		if(args === undefined){
 			return EMPTY_ARRAY;
 		}
-		if(typeof args === "string"){
+		if(utils.isString(args)){
 			var parsed = utils.parseColon(args);
 			if(parsed && parsed.prefix === "path"){
-				return [parsed.prefix+":"+AddBase(parsed.subfix)];
+				return [parsed.subfix];
 			}
 			return [args];
 		}
-		if(typeof args === "object"){
-			return Property(args);
+		if(utils.isArray(args)){
+			var tbr = [];
+			for(var i=0; i<args.length; i++){
+				tbr.push(ParseProperty(args[i]));
+			}
+			return tbr;
 		}
 	}
-	function AddBase(path){
-		if(base === undefined || base.length === 0){
-			return path;
-		}
-		return base + "." +path;
-	}
-	return{
-		path : AddBase(bd.path),
+	var tbr = {
+		path : bd.path,
 		scope: bd.scope,
 		args :  Arg(bd.args),
-		property : Property(bd.property)
+		property : Property(bd.property),
+		mixin : Arg(bd.mixin)
 	};
+
+	for(var name in bd){
+		if(!(name in tbr)){			
+			tbr[name] = bd[name];
+		}
+	}
+	return tbr;
 });
 
-define.module("factory.mapping", function(module,container){
+define.module("factory.mapping", function mapping(module,container){
 	var mapping = {};
 	return {
 		getBeanDefinition : function(id){
 			return mapping[id];
 		},
 		addBeanDefinition : function(){
-			var beanDefinitions = [];
-			var base = "";
-			beanDefLength = arguments.length;
-			if(typeof arguments[arguments.length-1] === "string"){
-				base = arguments[arguments.length-1];
-				beanDefLength--;
-			}
-			for(var i=0; i<beanDefLength; i++){
+			if(arguments[0].length !== undefined){
+				arguments = arguments[0]
+			}			
+			var beanDefLength = arguments.length;
+			for(var i=0; i < beanDefLength; i++){
 				var beanDefinition = arguments[i];
 				var id = beanDefinition.id;
 				if(id === undefined){
@@ -204,25 +234,20 @@ define.module("factory.mapping", function(module,container){
 				}
 				var exist = mapping[id];
 				if(exist !== undefined){ throw "bean : [" +id+"] already existed"};
-				mapping[id] = container.factory.BeanDefinition(beanDefinition, container.utils, base);
+				mapping[id] = container.factory.BeanDefinition(beanDefinition, container.utils);
 			}			
 			return this;
 		},
+		get : function(id){
+			return mapping[id];
+		}
 	};
 });
 
-define.clazz("factory.BeanGenerator", function BeanGenerator(id, container){
-	var mapping = container.factory.mapping;
+define.clazz("factory.BeanGenerator", function BeanGenerator(container){
 	var factory = container.factory;
-	function BeanNotDefined(innerId, parentId){
-				return "bean ["+innerId + "] has not been defined, and referenced in bean ["+parentId+"]";
-	}
-	var parentBeanDefinition = mapping.getBeanDefinition(id);
 	return {
 		create : function(beanDefinition){
-			if(beanDefinition === undefined){
-				beanDefinition = parentBeanDefinition;
-			}
 			if(typeof beanDefinition === "string"){
 				beanDefinition = factory.BeanDefinition({path:beanDefinition});
 			}
@@ -246,8 +271,21 @@ define.clazz("factory.BeanGenerator", function BeanGenerator(id, container){
 				if("path" in argBd){
 					return thisCreate(argBd);
 				}
+				if(container.utils.isArray(argBd)){
+					var arrayTbr = [];
+					for(var i=0; i<argBd.length; i++){
+						arrayTbr.push(getBean(argBd[i]));
+					}
+					return arrayTbr;
+				}
+				if(container.utils.isObject(argBd)){
+					return argBd;
+				}
 			}
-			var consturctor = container.utils.reflectedGet(container, beanDefinition.path);
+			var consturctor = container.utils.resolvePath(container, beanDefinition.path);
+			if(consturctor === undefined){
+				throw beanDefinition.path + " must be defined before referencing to it";
+			}
 			if(typeof consturctor === "function"){
 				var args = [];
 				var bdArgs = beanDefinition.args;
@@ -255,11 +293,26 @@ define.clazz("factory.BeanGenerator", function BeanGenerator(id, container){
 					var argBd = bdArgs[i];
 					args.push(getBean(argBd));
 				}
-				obj = consturctor.apply(undefined,args);
+				obj = consturctor.apply({},args);
+				if(obj === undefined){
+					throw "bean [" + beanDefinition.path + "] must not return null!";					
+				}
 				var properties = beanDefinition.property;
 				for(var name in properties){
 					var property = properties[name];
-					obj[name] = getBean(property);
+					var propBean = getBean(property);
+					if(typeof obj[name] === "function"){
+						propBean = propBean.length !== undefined? propBean : [propBean];
+						obj[name].apply(obj,[propBean])
+					}
+					else{
+						obj[name] = propBean;
+					}
+				}
+				var mixins = beanDefinition.mixin;
+				for(var name in mixins){
+					var mixinee = getBean(mixins[name]);
+					container.utils.merge(mixinee,obj);
 				}
 				return obj;
 			}
@@ -270,52 +323,127 @@ define.clazz("factory.BeanGenerator", function BeanGenerator(id, container){
 	};
 });
 
-define.module("factory", function(m, container){
+define.module("aop", function aop(m,container){
+	var mapping = container.factory.mapping;
+	var postProductionProcedure = (function(){
+		
+		var procedures = [];		
+		return {			
+			addProcedure : function(procedure){
+				procedures.push(procedure);
+			},			
+			advice : function (fnName, fn){
+				if(fnName === "get"){
+					return function p3Proxy(id){
+						var bean = fn(id);
+						var bd = mapping.getBeanDefinition(id);
+						for(var i=0; i < procedures.length; i++){
+							var modifiedBean = procedures[i](bean,bd);
+							bean = modifiedBean || bean;
+						}
+						return bean;
+					};
+				}				
+			}
+		}; 
+	})();
+	
+	return {
+		Interceptor : function(){
+			function makeProxy(fnName, fn, advice){
+				var adviced = advice(fnName, fn);
+				if(adviced === undefined || fn === adviced){
+					return fn;
+				}
+				adviced.adviced = true;
+				return adviced;
+			}
+			return {
+				intercept : function(obj, advice){
+					for(var name in obj){
+						if(typeof obj[name] === 'function'){
+							var proxied = makeProxy(name, obj[name], advice);
+							obj[name] = proxied;
+						}
+					}
+				}
+			};
+		},
+		p3Advice : postProductionProcedure
+	};
+});
+
+define.module("factory", function factory(m, container){
 	var scope =  (function(){
-		var singleton ={
-			"1" : true,
-			s : true,
-			S : true,
-			singleton : true
+		var prototype ={
+			P : true,
+			p : true,
+			prototype : true
 		}
 		var prototypeFactory = {
 			get : function(id){
-				return container.factory.BeanGenerator(id,container).create();
+				var bd = container.factory.mapping.get(id);
+				return container.factory.BeanGenerator(container).create(bd);
 			}
 		};
+		var interceptor = container.aop.Interceptor();
+		var p3Advice = container.aop.p3Advice.advice;
+		interceptor.intercept(prototypeFactory,p3Advice);
+		
 		var singletonFactory = (function(){
 			var store = {};
 			return {
 				get : function(id){
-					var singleton = store[id];
-					if(singleton !== undefined){
-						return singleton;
+					if(id in store){
+						return store[id];
 					}
-					store[id] = container.factory.BeanGenerator(id,container).create();
+					store[id] = prototypeFactory.get(id);
 					return store[id];
 				}
 			};
 		})();
-		return function(s){
-			return s in singleton ? singletonFactory : prototypeFactory;
+		return function(scope){
+			return scope in prototype ? prototypeFactory : singletonFactory;
 		};
-	})();	
+	})();
+
 	return {
 		getBean : function(id){
 			var beanDefinition = container.factory.mapping.getBeanDefinition(id);
 			if(beanDefinition === undefined){ throw "bean : [" +id+"] has not been defined"};
-			return scope(beanDefinition.scope).get(id);
+			return this.scope(beanDefinition.scope).get(id);
+		},
+		scope : scope,
+		registerP3 : function(fn){
+			container.aop.p3Advice.addProcedure(fn);			
 		}
 	};
 });
 
+define.module("factory", function DefaultPostProductionProcedure(m, container){	
+	var factory = container.factory;	
+	factory.registerP3(function initMethod(bean, bd){
+		var utils = container.utils;
+		if(bd.init === undefined){
+			return bean;
+		}
+		if(utils.isFunction(bean[bd.init])){
+			bean[bd.init]();
+			return;
+		}
+		throw new Error("object[" + utils.identity(bd) + "] does not have init method [" + bd.init +"]");
+	});
+});
+
+var jadi = {};
 define.module("publicInterface", function PublicInterface(m, container){
+	
 	return{
 		clazz : function(name, fn){
-			define.clazz("modules."+name,fn,false);
+			define.clazz(name,fn,false);
 		},
 		module : function(name, fn){
-			define.module("modules."+name,fn,false);
+			define.module(name,fn,false);
 		},
 		declareBeans : function(){
 			var mapping = container.factory.mapping;
@@ -323,14 +451,14 @@ define.module("publicInterface", function PublicInterface(m, container){
 			for(var i=0; i<arguments.length; i++){
 				definitions.push(arguments[i]);
 			}
-			definitions.push("modules");
 			mapping.addBeanDefinition.apply(mapping, definitions);
+			return definitions;
 		},
 		getBean : function(id){
 			return container.factory.getBean(id);
 		},
 		newInstance : function(beanDefinition){
-			return container.factory.BeanGenerator(undefined, container).create(beanDefinition);
+			return container.factory.BeanGenerator(container).create(beanDefinition);
 		},
 		require : function(beanIds, fn){
 			var beans = [];
@@ -338,7 +466,15 @@ define.module("publicInterface", function PublicInterface(m, container){
 				beans.push(this.getBean(beanIds[i]));
 			}
 			fn.apply({},beans);
+		},
+		mixin : function(src, des){
+			return container.utils.merge(src,des);
+		},
+		plugIn : function(fn){
+			return fn.apply(container,[]);
 		}
 	};
-}).bind(this,"jadi");
-})();
+}).bind(jadi,"newInstance");
+
+return jadi.newInstance;
+};
--- src/nodePlugIn.js
+++ src/nodePlugIn.js
@@ -1,8 +1,61 @@
-
-
-var jadi = require("./");
-
-jadi.plugIn(function(){
-	var container = this;
-	console.log(container);	
-});
+"use strict";
+var jadi = require("./jadi.js").jadi;
+exports.jadi = function(jadiInstance){
+	
+	jadiInstance = jadiInstance || jadi();
+	
+	return jadiInstance.plugIn(function(){
+		var container = this;
+		var originalResolve = container.utils.resolvePath;
+		var pathUtil = require('path');
+		container.utils.resolvePath = function(obj,path,parent){
+			var moduleClazz = path.split("@");
+			if(moduleClazz.length === 2){
+				var abPath = pathUtil.resolve(moduleClazz[0]);
+				var hasJs = abPath.indexOf(".js") !== -1;
+				if(!hasJs){
+					abPath = abPath+".js";
+				}
+				if(pathUtil.existsSync(abPath)){
+					var clazz = moduleClazz[1];
+					return require(abPath)[clazz];
+				}
+			}
+			return originalResolve(obj,path,parent);
+		};
+		
+		jadiInstance.nodeBeans = function(){
+			var mapping = container.factory.mapping;
+			var definitions = [];
+			for(var i=0; i<arguments.length; i++){
+				definitions.push(arguments[i]);
+			}
+			mapping.addBeanDefinition.apply(mapping, definitions);
+		};		
+		
+		jadiInstance.createServer = function(configFiles){
+			for(var i=0; i<configFiles.length; i++){
+				var filePath = pathUtil.resolve(configFiles[i]);
+				var beanDefinitions = require(filePath).beanDefinitions;
+				jadiInstance.nodeBeans(beanDefinitions);
+				for(var i=0; i<beanDefinitions.length; i++){
+					if(beanDefinitions[i].dispatcher){
+						if(beanDefinitions[i].id !==  undefined){
+							var dispatcher = jadiInstance.getBean(beanDefinitions[i].id);
+						}
+						else{
+							var dispatcher = jadiInstance.newInstance(beanDefinitions[i]);
+						}
+						break;
+					}
+				}
+				if(dispatcher === undefined){
+					throw "need to define a dispatcher";
+				}
+			}
+			var server=  require("http").createServer(dispatcher.start);
+			return server;
+		}		
+		return jadiInstance;
+	});
+}
