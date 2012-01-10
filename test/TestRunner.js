d843e72aee51c07b2fed33b4b7389db22f2080e2
= <yunxdev@gmail.com>
2012-01-09 20:40:04 -0800

    correct file pathes

--- test/TestRunner.js
+++ test/TestRunner.js
@@ -1,16 +1,16 @@
 "use strict";
 
-var jadiTest = require("./src/testPlugIn.js").jadiTest();
+var jadiTest = require("../src/testPlugIn.js").jadiTest();
 
 jadiTest.run({
 	id : "testPositive",
-	path : "./test/jadiTest@SomeTest",
+	path : "./jadiTest@SomeTest",
 	testSuit : "Sometest",
 	scope : "p"
 },
 {
 	id : "testPositive2",
-	path : "./test/jadiTest@SomeTest",
+	path : "./jadiTest@SomeTest",
 	testSuit : "Some test Else",
 	scope : "p"
 });
